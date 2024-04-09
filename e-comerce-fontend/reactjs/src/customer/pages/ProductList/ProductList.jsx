63bd4ec7ee4c2593d9572a3bd3cc2b18dee850c2
Tran Ty Phu Quy <20130377@st.hcmuaf.edu.vn>
2024-04-09 23:23:14 +0700

    setup layout product list page

--- e-comerce-fontend/reactjs/src/customer/pages/ProductList/ProductList.jsx
+++ e-comerce-fontend/reactjs/src/customer/pages/ProductList/ProductList.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Box, Container, Grid, Paper } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 
-Products.propTypes = {
-    
-};
+const useStyles = makeStyles(theme => ({
+    root:{},
 
-function Products(props) {
+    left:{width:'250px'},
+    right:{flex: '1 1 auto'}
+
+}))
+
+function ProductList(props) {
+    const classes = useStyles();
     return (
-        <div>
-            ProductList
-        </div>
+        <Box pt={4}>
+            <Container>
+                <Grid container spacing={1}>
+                    <Grid item className={classes.left}>
+                    <Paper elevation={0} />
+                        left column
+                    </Grid>
+                    <Grid item className={classes.right}>
+                    <Paper elevation={0} />
+                    right column
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     );
 }
 
-export default Products;
+export default ProductList;
