322fccd33bbc5f28b3ef0b82749b12c3f93ebb55
Tran Ty Phu Quy <20130377@st.hcmuaf.edu.vn>
2024-04-15 09:43:53 +0700

    fix code

--- e-comerce-fontend/src/App.js
+++ e-comerce-fontend/src/App.js
@@ -1,13 +1,10 @@
-
-import { Route, Routes } from 'react-router-dom';
-import CustomerRouters from './Routers/CustomerRouters';
-
-// Font and CSS
 import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Route, Routes } from 'react-router-dom';
+import CustomerRouters from './Routers/CustomerRouters';
 
 function App() {
   return (
