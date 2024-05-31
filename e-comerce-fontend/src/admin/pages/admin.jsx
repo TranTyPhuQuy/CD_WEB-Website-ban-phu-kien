ce3883249ccb5a4b50ee6b9a8c83ee7c5566c031
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2024-05-31 18:56:55 +0700

    create file admin

---
+++ e-comerce-fontend/src/admin/pages/admin.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// rsfp
+admin.propTypes = {
+    
+};
+
+function admin(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default admin;
